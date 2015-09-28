288b96ab1df92c7cb7afd86a663151efc8ef6df2
Alberto Nicoletti <albyx.n@gmail.com>
2015-09-28 16:36:14 +0200

    Cleanup

--- public/javascripts/virtual.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	document.getElementById('start').addEventListener('click', function () {
-	  var canvas = document.getElementById('screen');
-	  var screen = new Screen(canvas);
-	  client = new Client(screen);
-	  client.connect();
-	}, false);
-});
